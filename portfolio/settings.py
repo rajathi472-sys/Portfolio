fb5611f2ad763246cea0e726acb48afa6a7c3a65
rajathi472-sys <rasrajathi472@gmail.com>
2026-04-25 11:14:13 +0530

    Fix email port to 465

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -96,7 +96,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # ✅ Email (Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
